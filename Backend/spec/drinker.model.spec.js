30c1a9baf19ee29bc9083639d2921f91c76c1757
Michael Chan <michaelcychan@gmail.com>
2022-08-02 16:27:49 +0100

    added test mocking bcrypt when signing up

--- Backend/spec/drinker.model.spec.js
+++ Backend/spec/drinker.model.spec.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('./mongodb_helper');
 const Drinker = require('../models/drinker.model');
+const bcrypt = require('bcrypt');
 
 describe('Drinker', () => {
   beforeEach((done) => {
@@ -39,7 +40,44 @@ describe('Drinker', () => {
           bean_count: 0
         });
         done();
-      })
-    })
-  })
+      });
+    });
+  });
+
+  it('creates a user, mocks bcrypt', (done) => {
+    jest.spyOn(bcrypt, 'hash').mockImplementation((password, saltRounds, cb) => cb(null, 'mockHashedPassword'));
+    const req = {
+      body: {
+        firstname: 'First',
+        lastname: 'Last',
+        email: 'brewer@coffee.net',
+        password: 'inputPassword',
+      }
+    }
+    bcrypt.hash(req.body.password, 5, function(err, hashedPassword) {
+      const newDrinker = new Drinker({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        password: hashedPassword,
+        bean_count: 0
+      });
+
+      newDrinker.save((error) => {
+        expect(error).toBeNull();
+
+        Drinker.find((error, drinkers) => {
+          expect(error).toBeNull();
+          expect(drinkers[0]).toMatchObject({
+            firstname:  'First',
+            lastname: 'Last',
+            email: 'brewer@coffee.net',
+            password: 'mockHashedPassword',
+            bean_count: 0
+          });
+          done();
+        });
+      });
+    });
+  });
 })
