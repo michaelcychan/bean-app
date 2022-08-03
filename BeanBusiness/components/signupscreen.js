f260194483b15d4778615207c5c18b0d92ade0e5
abacus <abhiram0207@gmail.com>
2022-08-03 21:23:54 +0100

    fix google api functionality to work on signup screen

--- BeanBusiness/components/signupscreen.js
+++ BeanBusiness/components/signupscreen.js
@@ -22,59 +22,69 @@ export const SignupScreen = ({navigation}) => {
   return (
     <>
       <Text>Coffee Bean's Signup Page</Text>
-      <SafeAreaView>
-      <TextInput
-          style={styles.input}
-          onChangeText={onChangeShopName}
-          value={shopName}
-          placeholder="Enter your shop's name"
-          keyboardType="default"
+      <ScrollView>
+        <SafeAreaView>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeShopName}
+            value={shopName}
+            placeholder="Enter your shop's name"
+            keyboardType="default"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeShopEmail}
+            value={shopEmail}
+            placeholder="Enter your email address"
+            keyboardType="email-address"
+          />
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeShopPassword}
+            value={shopPassword}
+            placeholder="Enter your password"
+            keyboardType="default"
+            secureTextEntry={true}
+          />
+
+          <GooglePlacesAutocomplete
+            styles={{
+              container: {
+                flex: 1,
+              },
+              textInputContainer: {
+                backgroundColor: 'white',
+              },
+              textInput: {
+                height: 38,
+                fonSize: 16,
+              },
+              padding: 10,
+            }}
+            placeholder="Search"
+            minLength={2} // minimum length of text to search
+            autoFocus={true}
+            returnKeyType={'search'} // Can be left out for default return key
+            listViewDisplayed={false} // true/false/undefined
+            fetchDetails={true}
+            onPress={(data, details = null) => {
+              // 'details' is provided when fetchDetails = true
+              this.props.notifyChange(details.geometry.location);
+            }}
+            query={{
+              key: 'AIzaSyCC1_Bldu4Lmewp6bcUwhuOqWz8bwgEF1c',
+              language: 'en',
+            }}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={300}
+          />
+        </SafeAreaView>
+
+        <Button
+          title="Go to the login page"
+          onPress={() => navigation.navigate('Login', {name: 'LoginNow'})}
         />
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeShopEmail}
-          value={shopEmail}
-          placeholder="Enter your email address"
-          keyboardType="email-address"
-        />
-        <TextInput
-          style={styles.input}
-          onChangeText={onChangeShopPassword}
-          value={shopPassword}
-          placeholder="Enter your password"
-          keyboardType="default"
-          secureTextEntry={true}
-        />
-        <GooglePlacesAutocomplete
-          // eslint-disable-next-line react-native/no-inline-styles
-          styles={{
-            container: {
-              flex: 1,
-            },
-            textInputContainer: {
-              backgroundColor: 'grey',
-            },
-            textInput: {
-              height: 38,
-              fonSize: 16,
-            },
-            padding: 10,
-          }}
-          placeholder="Search"
-          onPress={(data, details = null) => {
-            console.log(data, details);
-            onChangeShopAddress(data) // is it the correct syntax??
-          }}
-          query={{
-            key: 'AIzaSyAAKl1eg50hAFGZrU2wIGwXeqRo-YlJJEY',
-            language: 'en',
-          }}
-        />
-      </SafeAreaView>
-      <Button
-        title="Go to the login page"
-        onPress={() => navigation.navigate('Login', {name: 'LoginNow'})}
-      />
+      </ScrollView>
     </>
   );
 };
