89aa1333e48fcf2b04f511e21ef2aae79ce69ac0
Dave Kempsell <dave_kempsell@hotmail.com>
2022-08-03 18:18:08 +0100

    adds pages, with links between them, and text inputs for sign up and sign in page

--- BeanFront/App.js
+++ BeanFront/App.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { View, Text, Button } from 'react-native';
-import { HomeScreen, ProfileScreen, HelloWorldScreen } from './components/Screens'
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { Home, SignUp, SignIn, Id, Beans, Settings } from './components/Screens'
 
 const Stack = createNativeStackNavigator();
 
@@ -12,14 +12,34 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen
           name="Home"
-          component={HomeScreen}
-          options={{ title: 'Welcome' }}
+          component={Home}
+          options={{ title: 'The Bean App' }}
         />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="Hello World" component={HelloWorldScreen}/>
+        <Stack.Screen 
+          name="Sign Up" 
+          component={SignUp} 
+          options={{ title: 'The Bean App'}}
+        />
+        <Stack.Screen
+          name="Sign In"
+          component={SignIn}
+          options={{ title: 'The Bean App' }}
+        />
+        <Stack.Screen 
+          name="Id" 
+          component={Id} 
+          options={{ title: 'The Bean App' }}
+        />
+        <Stack.Screen 
+          name="Beans" 
+          component={Beans} 
+          options={{ title: 'The Bean App' }}
+        />
+        <Stack.Screen name="Settings" component={Settings} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 };
+
 
 export default App;
--- BeanFront/components/Screens.js
+++ BeanFront/components/Screens.js
@@ -1,39 +1,144 @@
 import * as React from 'react';
-import { View, Button, Text } from 'react-native';
+import { View, Button, Text, StyleSheet } from 'react-native';
+import { FirstName, LastName, Email, Password } from './TextInputs'
 
-export const HomeScreen = ({ navigation }) => {
+export const Home = ({ navigation }) => {
   return (
     <View>
+      <Text>Receive free coffee</Text>
+      <Text>From your favourite independant coffee shop</Text>
     <Button
-      title="Go to Dave's profile"
+      style={styles.button}
+      title="Sign up for rewards"
       onPress={() =>
-        navigation.navigate('Profile', { name: 'Dave' })
+        navigation.navigate('Sign Up')
       }
     />
+    <Text>Already have an account?</Text>
     <Button
-      title="Go to Hello World"
+      title="Login"
       onPress={() =>
-        navigation.navigate('Hello World')
+        navigation.navigate('Sign In')
       }
     />
     </View>
   );
 };
 
-export const ProfileScreen = ({ navigation, route }) => {
+export const SignUp = ({ navigation, route }) => {
+
   return (
     <View>
-    <Text>This is {route.params.name}'s profile</Text>
-    <Button 
-      title="Hello World" 
-      onPress={() => 
-        navigation.navigate('Hello World')
-      }
-    />
+      <Text>Signup</Text>
+      {/* using components from TextInputs */}
+      <FirstName/>
+      <LastName/>
+      <Email/>
+      <Password/>
+      <Button title="Signup" />
+      <View>
+        <Text>Already have an account?</Text>
+        <Button 
+          title="Login" 
+          onPress={() => 
+            navigation.navigate('Sign In')
+          }
+        />
+      </View>
     </View>
   )
 };
 
-export const HelloWorldScreen = ({ navigation, route }) => {
-  return <Text>Hello World.</Text>
+export const SignIn = ({ navigation, route }) => {
+  return (
+    <View>
+      <Text>Login</Text>
+      {/* using components from TextInputs */}
+      <Email/>
+      <Password/>
+      <Button 
+            title="Login" 
+            onPress={() => 
+              navigation.navigate('Id')
+            }
+          />
+      <View>
+        <Text>Haven't signed up yet?</Text>
+        <Button
+          title="Sign up"
+          onPress={() =>
+            navigation.navigate('Sign Up')
+          }
+        />
+      </View>
+    </View>
+  )
 }
+
+export const Id = ({ navigation, route}) => {
+  return (
+    <View>
+    <Text>Member Number</Text>
+      <View>
+      <Text>0000-0001</Text>
+      </View>
+      <View>
+      <Button
+        title="Beans"
+        onPress={() =>
+          navigation.navigate('Beans')
+        }
+      />
+      </View>
+    <Button
+        title="Settings"
+        onPress={() =>
+          navigation.navigate('Settings')
+        }
+    />
+    </View>
+  )
+}
+
+export const Beans = ({ navigation, route}) => {
+  return (
+    <View>
+    <Text>Beans Collected</Text>
+      <View>
+        <Text>0</Text>
+      </View>
+    </View>
+  )
+}
+
+export const Settings = ({ navigation, route}) => {
+  return (
+    <Button 
+      title="Logout"
+      onPress={() =>
+        navigation.navigate('Home')
+      }
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginTop: 10,
+    marginBottom: 10
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  }
+})
