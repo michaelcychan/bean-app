53c33009d22f7464610bef9b258d9c425e7c974a
Michael Chan <michaelcychan@gmail.com>
2022-08-04 09:44:28 +0100

    implement auto incremental drinker_id to Drinker Schema with help from a counter database

--- Backend/models/drinker.model.js
+++ Backend/models/drinker.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const drinkercounter = require('./drinkercounter.helper');
 
 const Schema = mongoose.Schema;
 
@@ -30,6 +31,27 @@ const drinkerSchema = new Schema({
   bean_count: {
     type: Number,
   }
+});
+
+drinkerSchema.pre('save', function (done){
+  let doc = this;
+  drinkercounter.getSequenceNextValue("drinker_id")
+    .then(counter => {
+      console.log("found counter:", counter);
+      if(!counter) {
+        drinkercounter.insertCounter("drinker_id")
+          .then(counter => {
+            doc.drinker_id = counter;
+            console.log(doc);
+            done();
+          })
+          .catch(error => done(error))
+      } else {
+        doc.drinker_id = counter;
+        done();
+      }
+    })
+    .catch(error => done(error))
 });
 
 const Drinker = mongoose.model('Drinker', drinkerSchema);
