afe5c3641f30db397c131ce6fc25470b7076407f
Michael Chan <michaelcychan@gmail.com>
2022-08-04 11:19:48 +0100

    add more styles to stylesheet

--- BeanBusiness/components/homescreen.js
+++ BeanBusiness/components/homescreen.js
@@ -2,6 +2,7 @@ import React from 'react';
 import type {Node} from 'react';
 import {
   Button,
+  Pressable,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -18,15 +19,17 @@ export const HomeScreen = ({navigation}) => {
       <Text style={styles.title}>Loyal Bean</Text>
       <Text style={styles.subtitle}>Form relationships</Text>
       <Text>with loyal coffee drinkers</Text>
-      <Button
-        title="Start building your customer base"
+      <Pressable
+        style={styles.button}
         onPress={() => navigation.navigate('Signup', {name: 'SignupNow'})}
-      />
+      ><Text style={styles.buttontext}>Start building your customer base</Text>
+      </Pressable>
       <Text>Already have an account?</Text>
-      <Button
-        title="Go to the login page"
+      <Pressable
+        style={styles.button}
         onPress={() => navigation.navigate('Login', {name: 'LoginNow'})}
-      />
+      ><Text style={styles.buttontext}>Go to the login page</Text>
+      </Pressable>
     </View>
   );
 };
--- BeanBusiness/components/stylesheets.js
+++ BeanBusiness/components/stylesheets.js
@@ -21,4 +21,20 @@ export const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
   },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 4,
+    elevation: 3,
+    backgroundColor: 'black',
+  },
+  buttontext: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
 });
