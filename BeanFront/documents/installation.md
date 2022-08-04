28cc72066cc721ce8be01952c152b54867bb807d
Ibrahim Pala <96876777+IbrahimPala1@users.noreply.github.com>
2022-08-04 11:19:40 +0100

    deleted beanfront

--- BeanFront/documents/installation.md
+++
@@ -1,8 +0,0 @@
-```
-Ensure that cocoapods is working correctly. Within ios folder
-```
-
-gem install bundler
-brew install cocoapods
-brew link --overwrite cocoapods
-pod install
--- BeanFront/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import BeanApp from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => BeanApp);
