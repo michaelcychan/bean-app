ea47b5f55f274a2c75d9563f65b5c5b33382877f
Michael Chan <michaelcychan@gmail.com>
2022-08-05 19:16:11 +0100

    assign database object to a object to be showed at enterID.js

--- BeanBusiness/components/enterID.js
+++ BeanBusiness/components/enterID.js
@@ -3,26 +3,35 @@ import type {Node} from 'react';
 import {
   Button,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
   TextInput,
 } from 'react-native';
 import {styles} from './stylesheets';
 
 export const EnterID = ({navigation}) => {
-
-  const [drinkerID, onChangeDrinkerID] = React.useState(null);
-
+  const [drinkerIDInput, onChangeDrinkerIDInput] = React.useState(null);
+  const [drinkerObject, setDrinkerObject] = React.useState(null);
 
   const findDrinkerID = () => {
-    return fetch(`http://localhost:5050/barista/finddrinker/${drinkerID}`)
+    return fetch(`http://localhost:5050/barista/finddrinker/${drinkerIDInput}`)
       .then(response => response.json())
       .then(json => {
-        console.log(json.bean_count);
+        setDrinkerObject(json);
+        console.log(drinkerObject);
+        return json;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  const addBean = () => {
+    const bodyJSON = JSON.stringify(`drinker_id: ${drinkerID}`);
+    return fetch(`http://localhost:5050/barista/addBeans/${drinkerID}`)
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
         return json;
       })
       .catch(error => {
@@ -35,22 +44,27 @@ export const EnterID = ({navigation}) => {
       <SafeAreaView>
         <TextInput
           style={styles.input}
-          onChangeText={onChangeDrinkerID}
-          value={drinkerID}
+          onChangeText={onChangeDrinkerIDInput}
+          value={drinkerIDInput}
           placeholder="Enter customer's membership ID"
           keyboardType="numeric"
           maxLength={6}
         />
       </SafeAreaView>
-      <Button
-        title="Search User"
-        onPress={() =>
-          findDrinkerID()
-          // navigation.navigate('Customer Profile Screen', {
-          //   name: 'Customer Name Profile Page',
-          // })
-        }
-      />
+      <Button title="Search User" onPress={() => findDrinkerID()} />
+      <View>
+        {(() => {
+          if (drinkerObject != null) {
+            return (
+              <View>
+                <Text>Drinker Details: {drinkerObject.bean_count}</Text>
+                <Button title="Add a bean" onPress={() => addBean()} />
+                <Button title="Redeem a drink" />
+              </View>
+            )
+          }
+        })}
+      </View>
     </>
   );
 };
