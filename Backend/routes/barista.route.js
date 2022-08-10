b611f6de0c41741f6cfd284daaeb61416995a99a
Dave Kempsell <dave_kempsell@hotmail.com>
2022-08-10 13:15:56 +0100

    backend updates included

--- Backend/routes/barista.route.js
+++ Backend/routes/barista.route.js
@@ -3,7 +3,7 @@ const BaristaController = require('../controllers/barista.controller');
 
 router.post('/new-barista', BaristaController.CreateBarista);
 router.post('/log-in', BaristaController.BaristaLogIn);
-router.get('/finddrinker/:drinker_id', BaristaController.FindDrinker);
+router.post('/finddrinker', BaristaController.FindDrinker);
 router.post('/addbeans', BaristaController.AddLoyaltyBean);
 router.post('/redeemdrink/:drinker_id', BaristaController.RedeemDrink);
 router.post('/redeemdrink', BaristaController.NewRedeemDrink);
